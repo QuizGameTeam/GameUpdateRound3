f3642896d4b423b81b5e826e570deba809908199
TheWallOnFire <tuongmanhhuynh2005physic@gmail.com>
2023-08-16 14:17:41 +0700

    Update scene and state

--- Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/movement.cs
+++ Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/movement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public float moveSpeed = 5;
+    // public float maxSpeed = 10;
+    // public float acceleration = 2;
+    // public float deceleration = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // private Rigidbody2D rb;
+    // private Animator animator;
+
+    // private void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    //     animator = GetComponent<Animator>();
+    // }
+
+    // private void Update()
+    // {
+    //     // Get the current horizontal input
+    //     float horizontalInput = Input.GetAxis("Horizontal");
+
+    //     // Accelerate the player
+    //     rb.AddForce(new Vector2(horizontalInput * acceleration, 0));
+
+    //     // Limit the player's speed to the maximum speed
+    //     if (rb.velocity.x > maxSpeed)
+    //     {
+    //         rb.velocity.x = maxSpeed;
+    //     }
+
+    //     // Slow down the player after releasing the button
+    //     if (horizontalInput == 0 && rb.velocity.x > 0)
+    //     {
+    //         rb.velocity.x -= deceleration;
+    //     }
+
+    //     // Update the animation
+    //     animator.SetFloat("MoveX", rb.velocity.x);
+    // }
+
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     // If the player collides with a wall, stop them moving
+    //     if (collision.gameObject.tag == "Wall")
+    //     {
+    //         rb.velocity.x = 0;
+    //     }
+    // }
 }
