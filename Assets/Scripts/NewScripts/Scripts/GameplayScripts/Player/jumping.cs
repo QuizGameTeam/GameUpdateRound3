7909171a67e712edbd3bcd08283a6ae20b78a718
TheWallOnFire <tuongmanhhuynh2005physic@gmail.com>
2023-08-17 12:46:04 +0700

    New controller

--- Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/jumping.cs
+++ Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/jumping.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class jumping : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool grounded = false;
+    public float jumpForce = 10;
+    // public float groundCheckDistance;
+    // private float bufferCheckDistance = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // groundCheckDistance = (GetComponent<CapsuleCollider2D>().size.y/2) + bufferCheckDistance;
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+        // RaycastHit hit;
+        // if (Physics.Raycast(transform.position, -transform.up, out hit, groundCheckDistance))
+        // {
+        //     grounded = true;
+        // }
+        // else
+        // {
+        //     grounded = false;
+        // }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        grounded = false;
     }
 }
--- Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/movement.cs
+++ Assets/Scripts/NewScripts/Scripts/GameplayScripts/Player/movement.cs
@@ -1,53 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
+
+// MOVEMENT OF PLAYER
+
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // public float moveSpeed = 5;
-    // public float maxSpeed = 10;
-    // public float acceleration = 2;
-    // public float deceleration = 1;
+    public float Speed = 10;
+    public float gravity = 10;
 
-    // private Rigidbody2D rb;
-    // private Animator animator;
+    private Rigidbody2D rb;
+    private Animator animator;
 
-    // private void Start()
-    // {
-    //     rb = GetComponent<Rigidbody2D>();
-    //     animator = GetComponent<Animator>();
-    // }
+    public bool grounded = false;
+    public float jumpForce = 10;
+    // public float groundCheckDistance;
+    // private float bufferCheckDistance = 0.1f;
 
-    // private void Update()
-    // {
-    //     // Get the current horizontal input
-    //     float horizontalInput = Input.GetAxis("Horizontal");
+    void Start()
+    {
+        // Difference character ability
+        if (gameObject.name == "Jumper")
+        {
+            // Jump higher
+            Speed = 10;
+            gravity = 10;
+            jumpForce = 10;
+        }
+        else if (gameObject.name == "Runner")
+        {
+            // Run faster
+            Speed = 10;
+            gravity = 10;
+            jumpForce = 10;
+        }
+        else if (gameObject.name == "Flyer")
+        {
+            // Lower gravity
+            Speed = 10;
+            gravity = 10;
+            jumpForce = 10;
+        }
 
-    //     // Accelerate the player
-    //     rb.AddForce(new Vector2(horizontalInput * acceleration, 0));
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
-    //     // Limit the player's speed to the maximum speed
-    //     if (rb.velocity.x > maxSpeed)
-    //     {
-    //         rb.velocity.x = maxSpeed;
-    //     }
+    void FixedUpdate()
+    {
+        rb.gravityScale = gravity;
 
-    //     // Slow down the player after releasing the button
-    //     if (horizontalInput == 0 && rb.velocity.x > 0)
-    //     {
-    //         rb.velocity.x -= deceleration;
-    //     }
+        // Horizontal movement
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0)
+        {
+            rb.AddForce(new Vector2(horizontalInput * Speed, 0f));
+            gameObject.transform.localScale = new Vector3(horizontalInput,1,1);
+        }
+    }
 
-    //     // Update the animation
-    //     animator.SetFloat("MoveX", rb.velocity.x);
-    // }
+    // Update is called once per frame
+    void Update()
+    {
+        // Jump method
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
 
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     // If the player collides with a wall, stop them moving
-    //     if (collision.gameObject.tag == "Wall")
-    //     {
-    //         rb.velocity.x = 0;
-    //     }
-    // }
+        // groundCheckDistance = (GetComponent<CapsuleCollider2D>().size.y/2) + bufferCheckDistance;
+        // RaycastHit hit;
+        // if (Physics.Raycast(transform.position, -transform.up, out hit, groundCheckDistance))
+        // {
+        //     grounded = true;
+        // }
+        // else
+        // {
+        //     grounded = false;
+        // }
+    }
+
+    // Check collision
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        grounded = false;
+    }
 }
