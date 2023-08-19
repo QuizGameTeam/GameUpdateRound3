05ed5d6b3a8c665a82679eab5225fd1aea532ef7
TheWallOnFire <tuongmanhhuynh2005physic@gmail.com>
2023-08-19 23:01:36 +0700

    heathbar added

--- Assets/Scripts/OldScripts/UI/Score.cs
+++ Assets/Scripts/OldScripts/UI/Score.cs
@@ -1,32 +1,32 @@
 
-// Show final score
+// // Show final score
 
-using UnityEngine;
-using TMPro;
-using System;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using section;
+// using UnityEngine;
+// using TMPro;
+// using System;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+// using section;
 
 
 
-public class Score : MonoBehaviour
-{
-    private Text ScoreNum;
-    private gameManager gamemanager;
-    private int ScoreFinal;
+// public class Score : MonoBehaviour
+// {
+//     private Text ScoreNum;
+//     private gameManager gamemanager;
+//     private int ScoreFinal;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ScoreNum = GetComponent<Text>();
-        gamemanager = FindObjectOfType<gameManager>();
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         ScoreNum = GetComponent<Text>();
+//         gamemanager = FindObjectOfType<gameManager>();
         
 
-    }
-    void Update() {
-        ScoreFinal = gamemanager.MyScore;
-        // ScoreNum.text = " Score: " + PlayerPrefs.GetInt("Score").ToString();
-        ScoreNum.text = " Score: " + ScoreFinal.ToString();
-    }
-}
+//     }
+//     void Update() {
+//         ScoreFinal = gamemanager.MyScore;
+//         // ScoreNum.text = " Score: " + PlayerPrefs.GetInt("Score").ToString();
+//         ScoreNum.text = " Score: " + ScoreFinal.ToString();
+//     }
+// }
